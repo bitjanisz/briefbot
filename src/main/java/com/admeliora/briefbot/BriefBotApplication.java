7464cdadb362d01dbba04b44d60fcc19ca91906f
BitJanisz <janisz1991@gmail.com>
2025-10-27 14:11:33 +0100

    first commit

---
+++ src/main/java/com/admeliora/briefbot/BriefBotApplication.java
@@ -0,0 +1,13 @@
+package com.admeliora.briefbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admeliora/briefbot/BriefBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admeliora.briefbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
