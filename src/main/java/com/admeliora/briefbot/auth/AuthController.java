edbf417db8ec7e9e8b13318b2f9a9f6cc39d910f
BitJanisz <janisz1991@gmail.com>
2025-12-10 19:45:37 +0100

    BRI-27: adds user registration

--- src/main/java/com/admeliora/briefbot/auth/AuthController.java
+++ src/main/java/com/admeliora/briefbot/auth/AuthController.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     @Transactional
