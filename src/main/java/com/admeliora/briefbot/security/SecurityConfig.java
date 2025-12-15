25f85f9f066ab0d9a3c00a24efd60e634943c2df
BitJanisz <janisz1991@gmail.com>
2025-12-15 21:51:51 +0100

    adds logout with h2 handler

--- src/main/java/com/admeliora/briefbot/security/SecurityConfig.java
+++ src/main/java/com/admeliora/briefbot/security/SecurityConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Slf4j
 @Configuration
@@ -15,14 +18,36 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                            OidcAuthenticationSuccessHandler successHandler) throws Exception {
+                                            OidcAuthenticationSuccessHandler successHandler,LogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/health","/h2-console/**").permitAll()
                         .anyRequest().authenticated()
+                ).csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
                 )
-                .oauth2Login(o -> o.successHandler(successHandler));
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin())
+                )
+                .oauth2Login(o -> o.successHandler(successHandler))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler) // <-- changed line
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                );
         return http.build();
+    }
+
+    @Bean
+    LogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        successHandler.setPostLogoutRedirectUri("{baseUrl}/");
+
+        return successHandler;
     }
 
     @Bean
