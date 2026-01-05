c71eba98f1a39d867464e9727a4f71c6d79c371b
BitJanisz <janisz1991@gmail.com>
2026-01-05 12:08:39 +0100

    adds bean

--- app/src/main/java/com/admeliora/briefbot/security/SecurityConfig.java
+++ app/src/main/java/com/admeliora/briefbot/security/SecurityConfig.java
@@ -5,36 +5,25 @@ import com.admeliora.briefbot.adapter.out.persistence.user.jpa.UserRepositoryJpa
 import com.admeliora.briefbot.security.jwt.JwtAuthenticationFilter;
 import com.admeliora.briefbot.security.jwt.JwtProperties;
 import com.admeliora.briefbot.security.jwt.JwtTokenProvider;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseCookie;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -43,30 +32,54 @@ public class SecurityConfig {
     @Value("${security.redirect.login-url:/login}")
     private String loginUrl;
 
+//    @Bean
+//    @Order(1)
+//    SecurityFilterChain managementSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatcher(request -> request.getLocalPort() == 8081)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//                                "/h2-console/**",
+//                                "/actuator/**"
+//                        ).permitAll()
+//                        .anyRequest().denyAll()
+//                )
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+//                .exceptionHandling(exception -> exception
+//                        .defaultAuthenticationEntryPointFor(
+//                                (request, response, authException) -> response.sendError(403, "Forbidden"),
+//                                request -> true
+//                        )
+//                );
+//        return http.build();
+//    }
+
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http,
                                            OidcAuthenticationSuccessHandler oidcSuccessHandler,
                                            JwtAuthenticationFilter jwtAuthenticationFilter,
                                            JwtProperties jwtProperties) {
         try {
             http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/", "/index.html", "/test-page/**","/other/**", "/login/**", "/assets/**", "/vite.svg", "/static/**").permitAll()
-                    .requestMatchers("/api/users/**").permitAll()
-                    .requestMatchers("/api/sample").permitAll()
-                    .anyRequest().authenticated()
-                )
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/", "/index.html", "/test-page/**", "/other/**", "/login/**", "/assets/**", "/vite.svg", "/static/**").permitAll()
+                            .requestMatchers("/api/users/**").permitAll()
+                            .requestMatchers("/api/sample").permitAll()
+                            .anyRequest().authenticated()
+                    )
                     .sessionManagement(session -> session
                             .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless for JWT
                     )
                     .headers(headers -> headers
                             .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                     )
-                .oauth2Login(oauth -> oauth
-                    .loginPage(loginUrl)
-                        .successHandler(oidcSuccessHandler)
-                )
+                    .oauth2Login(oauth -> oauth
+                            .loginPage(loginUrl)
+                            .successHandler(oidcSuccessHandler)
+                    )
                     .logout(logout -> logout
                             .logoutUrl("/logout")
                             .invalidateHttpSession(true)
